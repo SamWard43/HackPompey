bc5acbd4a0d733e8133e632031278a1aa1a2a582
SamWard43 <44724948+SamWard43@users.noreply.github.com>
2018-11-03 15:25:31 +0000

    Organising files

---
+++ sourceCode/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+
+        
+        float _horizontal = Input.GetAxisRaw("Horizontal");
+        float _vertical = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 movement = new Vector3(_horizontal, 0f, _vertical);
+
+        rb.AddForce(movement * speed * Time.deltaTime);
+    }
+}
+ 
+
